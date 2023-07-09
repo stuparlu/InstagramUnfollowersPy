37ef714a7f279a54c8725f18694c068282088bcc
Luka Stupar <stupar22luka@gmail.com>
2023-07-09 15:39:13 +0200

    Inital job

---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+
+from utils.constants import followers_file_path, following_file_path, folowing_key, list_data_key, value_key, \
+    output_non_follower_file_path, opening_html_literal, closing_html_literal
+
+
+def main():
+    with open(followers_file_path, 'r') as file:
+        followers_data = json.load(file)
+
+    with open(following_file_path, 'r') as file:
+        following_data = json.load(file)[folowing_key]
+
+    following_data = [obj[list_data_key][0] for obj in following_data]
+    followers_data = [obj[list_data_key][0] for obj in followers_data]
+    filtered_list = [element for element in following_data if element[value_key] not in [item[value_key] for item in followers_data]]
+
+    print(filtered_list)
+    html = opening_html_literal
+    html += "".join(f'<a href="{item["href"]}">{index+1}. {item["value"]}</a><br>' for index, item in enumerate(filtered_list))
+    html += closing_html_literal
+
+    with open(output_non_follower_file_path, "w") as file:
+        file.write(html)
+
+if __name__ == "__main__":
+    main()
---
+++ utils/constants.py
@@ -0,0 +1,12 @@
+followers_file_path = 'followers_and_following/followers.json'
+following_file_path = 'followers_and_following/following.json'
+output_non_follower_file_path = 'output/output.html'
+
+
+folowing_key = 'relationships_following'
+list_data_key = 'string_list_data'
+value_key = 'value'
+
+
+opening_html_literal = "<html><body>"
+closing_html_literal = "</body></html>"
